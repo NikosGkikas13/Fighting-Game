595e50e1c40b3215976d5d78317efb2c43dec074
Nikos Gkikas <69345580+NikosGkikas13@users.noreply.github.com>
2022-05-20 13:48:29 +0300

    created players classes

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-console.log("hi");
+const canvas = document.querySelector("canvas");
+const c = canvas.getContext("2d");
+
+canvas.width = 1024;
+canvas.height = 576;
+c.fillRect(0, 0, canvas.width, canvas.height);
+
+class Sprite {
+  constructor({ position, velocity }) {
+    this.position = position;
+    this.velocity = velocity;
+  }
+  draw() {
+    c.fillStyle = "red";
+    c.fillRect(this.position.x, this.position.y, 50, 150);
+  }
+}
+
+const player1 = new Sprite({
+  position: { x: 0, y: 0 },
+  velocity: { x: 0, y: 0 },
+});
+const player2 = new Sprite({
+  position: { x: 500, y: 400 },
+  velocity: { x: 0, y: 0 },
+});
+
+player1.draw();
+player2.draw();
+
+const animate = () => {
+  window.requestAnimationFrame(animate);
+  console.log("hi");
+};
+animate();
